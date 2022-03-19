06b058dc845ce4b5171d1ab2cba8ce99d0369aea
AlbertHuang <r.albert.huang@gmail.com>
2022-03-19 12:08:19 +0800

    fix: deleted twteet when update score

--- internal/pkg/tw/score.go
+++ internal/pkg/tw/score.go
@@ -38,9 +38,10 @@ func (t *Tweet) UpdateTweetScore() error {
 		if err != nil {
 			return err
 		}
-		if tweetResponse.Raw == nil {
-			return fmt.Errorf("no tweet found %d", tweet.Id)
+		if tweetResponse.Raw.Errors != nil {
+			return fmt.Errorf("%s", tweetResponse.Raw.Errors[0].Detail)
 		}
+
 		dictionaries := tweetResponse.Raw.TweetDictionaries()
 		for _, t := range dictionaries {
 			tweet.LikeCount = t.Tweet.PublicMetrics.Likes
